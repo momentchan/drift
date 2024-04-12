1ade20a26477b61f4a97564d48e95bc2d32f2c10
momentchan <mingjyunhung@gmail.com>
2024-04-12 15:01:26 +0800

    update

--- src/shaders/boidsMeshRenderShader.js
+++ src/shaders/boidsMeshRenderShader.js
@@ -14,7 +14,6 @@ export default class BoidsMeshRenderShader extends THREE.ShaderMaterial {
 
             ${utility}
 
-
             void main() {
                 vec3 pos = texture2D(positionTex, uvs.xy).xyz;
                 vec3 vel = texture2D(velocityTex, uvs.xy).xyz;
@@ -26,33 +25,14 @@ export default class BoidsMeshRenderShader extends THREE.ShaderMaterial {
 
                 vec4 newPos = rotMat * vec4(position, 1.0);
 
-                // vec4 rotatedPos = rotMat * vec4(position, 0.0); 
-
-
-                // vec4 mvPosition = modelViewMatrix * vec4(pos, 1.0);
-                // gl_Position = projectionMatrix * mvPosition;
-                // vDistance = abs(mvPosition.z);
-
-                // gl_PointSize = 10.0 * mix(0.2, 1.0, pow(smoothstep(80.0, 0.0, vDistance), 2.0));
-
                 gl_Position = projectionMatrix * modelViewMatrix * instanceMatrix * vec4( newPos.xyz + pos.xyz, 1.0 );
-            }
-            
-            `,
+            }`,
 
 
             fragmentShader: /* glsl */ `
             varying float vDistance;
 
             void main() {
-                // vec2 cxy = 2.0 * gl_PointCoord - 1.0;
-                // if(dot(cxy, cxy) > 1.0) discard;
-
-                // vec3 color = vec3(1.0);
-
-                // gl_FragColor = vec4(color, mix(0.0, 1.0, pow(smoothstep(50.0, 0.0, vDistance), 2.0)));
-
-
                 gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
             }`,
 
