452aa1d89d105b5cfc1175894e63f0594af124c3
momentchan <mingjyunhung@gmail.com>
2024-08-12 22:42:25 +0800

    update

--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-import './style.css'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import { StrictMode } from 'react'
-
-createRoot(document.querySelector('#root')).render(<App />)
