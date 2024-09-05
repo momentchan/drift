5d6cc54f419f3ac8dc7434805e7fa64ec4c06611
momentchan <mingjyunhung@gmail.com>
2024-09-05 13:08:18 +0900

    refactor ray

--- src/RayEmitter.jsx
+++ src/RayEmitter.jsx
@@ -4,66 +4,46 @@ import * as THREE from 'three';
 import { Line } from '@react-three/drei';
 import { randFloatSpread } from 'three/src/math/MathUtils.js';
 import GlobalState from './GlobalState';
+import gsap from 'gsap';
 
 const rfs = THREE.MathUtils.randFloatSpread
 const speedRange = [-0.3, -0.8]
 const lengthRange = [5, 20]
 const delayRange = [0, -10]
 
+function Shape({ points, ratio }) {
+    return (
+        points.length !== 0 && ratio !== 0 &&
+        <Line points={points} color="white" transparent opacity={1 - ratio} lineWidth={1} />
+    );
+}
+
 function Triangle({ pos, ratio }) {
     const [points, setPoints] = useState([]);
     const size = useMemo(() => THREE.MathUtils.randFloat(5, 15), [])
-    const [fade, setFade] = useState(1)
     const dir = useMemo(() => new THREE.Vector3(randFloatSpread(1), randFloatSpread(1), randFloatSpread(1)).normalize(), [])
 
-    const calculatePoints = () => {
+    useEffect(() => {
         const angle = Math.PI * 2 / 3
-        setFade(THREE.MathUtils.smoothstep(1 - ratio, 0, 0.3))
-
         const p1 = new THREE.Vector3();
         p1.crossVectors(dir, new THREE.Vector3(1, 0, 0)).multiplyScalar(size * Math.pow(ratio, 0.5));
 
         const p2 = p1.clone().applyAxisAngle(dir, angle);
         const p3 = p1.clone().applyAxisAngle(dir, -angle);
-
         const points = [p1.clone().add(pos), p3.clone().add(pos), p2.clone().add(pos), p1.clone().add(pos)];
         setPoints(points);
-    };
+    }, [ratio])
 
-    useFrame((state) => {
-        calculatePoints()
-    })
-
-    const lineRef = useRef()
-    useEffect(() => {
-        if (lineRef.current) {
-            lineRef.current.layers.toggle(2)
-            // console.log(lineRef.current.layers);
-        }
-    }, [lineRef.current])
-
-    return <>
-        {points.length != 0 && ratio != 0 ?
-            <Line
-                ref={lineRef}
-                points={points}
-                color="white"
-                transparent
-                opacity={fade}
-                lineWidth={1} /> :
-            ""}
-    </>
+    return <Shape points={points} ratio={ratio} />;
 }
 
 function Rectangle({ pos, ratio }) {
     const [points, setPoints] = useState([]);
     const size = useMemo(() => THREE.MathUtils.randFloat(5, 15), [])
-    const [fade, setFade] = useState(1)
     const dir = useMemo(() => new THREE.Vector3(randFloatSpread(1), randFloatSpread(1), randFloatSpread(1)).normalize(), [])
 
-    const calculatePoints = () => {
+    useEffect(() => {
         const angle = Math.PI / 2
-        setFade(THREE.MathUtils.smoothstep(1 - ratio, 0, 0.3))
 
         const p1 = new THREE.Vector3();
         p1.crossVectors(dir, new THREE.Vector3(1, 0, 0)).multiplyScalar(size * Math.pow(ratio, 0.5));
@@ -74,56 +54,31 @@ function Rectangle({ pos, ratio }) {
 
         const points = [p1.clone().add(pos), p2.clone().add(pos), p3.clone().add(pos), p4.clone().add(pos), p1.clone().add(pos)];
         setPoints(points);
-    };
+    }, [ratio])
 
-    useFrame((state) => {
-        calculatePoints()
-    })
-
-    return <>
-        {points.length != 0 && ratio != 0 ?
-            <Line
-                points={points}
-                color="white"
-                transparent
-                opacity={fade}
-                lineWidth={1} /> :
-            ""}
-    </>
+    return <Shape points={points} ratio={ratio} />;
 }
 
-function Ray({ index, pos, dir, normal, binormal, lengthRange, speedRange, range = 30, onUpdatePoints }) {
-
+function Ray({ index, pos, dir, normal, binormal, lengthRange, speedRange, onUpdatePoints }) {
     const { started } = GlobalState();
-
-
     const [delay, setDelay] = useState(THREE.MathUtils.randFloat(delayRange[0], delayRange[1]))
     const [speed, setSpeed] = useState(THREE.MathUtils.randFloat(speedRange[0], speedRange[1]))
     const [length, setLength] = useState(THREE.MathUtils.randFloat(lengthRange[0], lengthRange[1]))
     const [fade, setFade] = useState(0)
     const [stopPos, setStopPos] = useState(0)
-    const [stop, setStop] = useState(false)
-    const [triPos, setTriPos] = useState([new THREE.Vector3(-100, -100, -100), new THREE.Vector3(-100, -100, -100)])
     const shape = useMemo(() => THREE.MathUtils.randFloat(0, 1) > 0.5, [])
-
     const velocity = useMemo(() => dir.clone().multiplyScalar(speed), [dir, speed]);
-
     const [points, setPoints] = useState([new THREE.Vector3(-100, -100, -100), new THREE.Vector3(-100, -100, -100)])
-
-    const [time, setTime] = useState(0)
     const [duration, setDuration] = useState(THREE.MathUtils.randFloat(2, 5))
-
-    const [sound, setSound] = useState(false)
+    const [spawned, setSpawned] = useState(false)
     const { camera } = useThree()
     const soundRef = useRef();
+    const [shapeRatio, setShapeRatio] = useState(0)
 
-    function getRandomPos() {
-        setDuration(THREE.MathUtils.randFloat(2, 5))
-        setTime(0)
-        setStop(false)
-        setSound(false)
-        setTriPos([new THREE.Vector3(-100, -100, -100), new THREE.Vector3(-100, -100, -100)])
-
+    function randomizePosition() {
+        setDuration(THREE.MathUtils.randFloat(2, 5));
+        setSpawned(false);
+        setShapeRatio(0);
         setLength(THREE.MathUtils.randFloat(lengthRange[0], lengthRange[1]))
         setSpeed(THREE.MathUtils.randFloat(speedRange[0], speedRange[1]))
         setDelay(THREE.MathUtils.randFloat(delayRange[0], delayRange[1]))
@@ -137,8 +92,29 @@ function Ray({ index, pos, dir, normal, binormal, lengthRange, speedRange, range
         setPoints([p.clone(), p.clone().add(dir.clone().multiplyScalar(length))])
     }
 
+    const spawnShape = () => {
+        const ratio = { value: 0 };
+        gsap.to(ratio, {
+            value: 1,
+            duration: duration,
+            ease: "Power2.easeOut",
+            onStart: () => {
+                setPoints([points[0], points[0]]);
+                soundRef.current.position.copy(points[1]);
+                soundRef.current.play();
+            },
+
+            onUpdate: () => {
+                setShapeRatio(ratio.value)
+            },
+            onComplete: () => {
+                randomizePosition();
+            }
+        });
+    }
+
     useEffect(() => {
-        getRandomPos()
+        randomizePosition()
     }, [])
 
     useEffect(() => {
@@ -159,57 +135,43 @@ function Ray({ index, pos, dir, normal, binormal, lengthRange, speedRange, range
     }, [started])
 
     useFrame((state, delta) => {
-        if (!started) return
+        if (!started) return;
 
-        setDelay(delay + Math.min(delta, 1 / 30))
+        setDelay(delay + Math.min(delta, 1 / 30));
+
         if (delay > 0) {
+            // move
+            if (length > 0) {
+                const p1 = points[0].add(velocity)
+                const p2 = p1.clone().add(velocity.clone().normalize().multiplyScalar(length))
+                setPoints([p1, p2])
 
-            const dot = points[0].dot(dir.clone().normalize())
+                // fade-in
+                setFade(THREE.MathUtils.clamp(delay / 10, 0, 1) * THREE.MathUtils.smootherstep(points[1].y, -50, -30))
 
-            setFade(THREE.MathUtils.clamp(delay / 10, 0, 1) * THREE.MathUtils.smootherstep(points[1].y, -50, -30))
-
-            if (dot < stopPos) {
-                setLength(Math.max(length - velocity.length(), 0))
-
-                if (!stop) {
-                    setTriPos(points[1])
-                    setStop(true)
+                // adjust length
+                const dot = points[0].dot(dir.clone().normalize())
+                if (dot < stopPos) {
+                    setLength(Math.max(length - velocity.length(), 0))
                 }
+
+                onUpdatePoints(index, points[0], length)
             }
 
-            const p1 = points[0].add(velocity)
-            const p2 = p1.clone().add(velocity.clone().normalize().multiplyScalar(length))
-            setPoints([p1, p2])
-
-            onUpdatePoints(index, points[0], length)
-
-            if (length == 0) {
-                setTime(time + delta)
-
-                if (!sound) {
-                    soundRef.current.position.copy(p1);
-                    soundRef.current.play();
-                    setSound(true);
+            // spawned
+            else {
+                if (!spawned) {
+                    spawnShape();
+                    setSpawned(true);
                 }
-            }
-
-            if (points[1].y < -100) {
-                getRandomPos()
             }
         }
     });
 
     return (
         <>
-            {delay < 0 ? "" : <Line
-                points={points}
-                color="white"
-                lineWidth={1}
-                transparent
-                opacity={fade}
-            />}
-
-            {shape ? <Rectangle pos={triPos} ratio={Math.min(time / duration, 1)} /> : <Triangle pos={triPos} ratio={Math.min(time / duration, 1)} />}
+            {delay >= 0 && <Line points={points} color="white" lineWidth={1} transparent opacity={fade} />}
+            {shape ? <Rectangle pos={points[1].clone()} ratio={shapeRatio} /> : <Triangle pos={points[1].clone()} ratio={shapeRatio} />}
         </>
     );
 }
@@ -220,25 +182,23 @@ export default function RayEmitter({ rayCount, lightPos, onUpdateTexture }) {
     const [binormal, setBinormal] = useState(new THREE.Vector3());
     const pos = new THREE.Vector3(lightPos[0], lightPos[1], lightPos[2])
 
-
     useEffect(() => {
-        const newDir = new THREE.Vector3().copy(pos).normalize();
-        setDir(newDir);
+        const direction = new THREE.Vector3().copy(pos).normalize();
+        setDir(direction);
 
-        const newNormal = new THREE.Vector3();
-        newNormal.crossVectors(newDir, new THREE.Vector3(1, 0, 0)).normalize();
+        const normalVector = new THREE.Vector3();
+        normalVector.crossVectors(direction, new THREE.Vector3(1, 0, 0)).normalize();
+        setNormal(normalVector);
 
-        const newBinormal = new THREE.Vector3();
-        newBinormal.crossVectors(newDir, newNormal).normalize();
-
-        setNormal(newNormal);
-        setBinormal(newBinormal);
-    }, []);
+        const binormalVector = new THREE.Vector3();
+        binormalVector.crossVectors(direction, normalVector).normalize();
+        setBinormal(binormalVector);
+    }, [lightPos]);
 
     return (
         <>
-            {normal.length() != 0 &&
-                Array.from({ length: rayCount }, (_, i) => (
+            {normal.length() > 0 &&
+                Array.from({ length: rayCount }).map((_, i) => (
                     <Ray
                         key={i}
                         index={i}
